86ab705a97923473a715f36278fb09d41ca511eb
Usif Ahmed <usifahmedmuhammed@gmail.com>
2023-12-06 12:41:28 +0200

    Project structure

---
+++ cmd/api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cnfg config
+
+	flag.IntVar(&cnfg.port, "port", 3000, "API server port")
+	flag.StringVar(&cnfg.env, "env", "development", "Envirnoment -> Dev|Staging|Prod")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cnfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cnfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cnfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/movapi
+
+go 1.21.1
