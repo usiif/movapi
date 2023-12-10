27bf740cd03b7fff44fa14147d5e4daf3d2b36ba
Usif Ahmed <usifahmedmuhammed@gmail.com>
2023-12-10 04:53:40 +0200

    JSON Error Handling

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -9,7 +9,21 @@ import (
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "CREATING MOVIE...")
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.logger.Println(err)
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
